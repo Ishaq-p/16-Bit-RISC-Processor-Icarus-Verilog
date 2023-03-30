b507a307c374a1a175f7766357634983d398a37f
adnan <adnanyu>
2023-03-31 02:37:39 +0300

    changed the initiation in the register file from initail to always and changed the alu and alucontrolunit from 3bit to 4bit

--- 16_Bit_RISC_Processor_Icarus-Verilog_Code/ALU.v
+++ 16_Bit_RISC_Processor_Icarus-Verilog_Code/ALU.v
@@ -3,7 +3,7 @@
 // Verilog code for RISC Processor 
 // Verilog code for ALU
 module ALU(
- input  [15:0] c,  //src1
+ input  [15:0] a,  //src1
  input  [15:0] b,  //src2
  input  [2:0] alu_control, //function sel
  
@@ -14,14 +14,14 @@ module ALU(
 always @(*)
 begin 
  case(alu_control)
- 3'b000: result = a + b; // add
- 3'b001: result = a - b; // sub
- 3'b010: result = ~a;
- 3'b011: result = a<<b;
- 3'b100: result = a>>b;
- 3'b101: result = a & b; // and
- 3'b110: result = a | b; // or
- 3'b111: begin if (a<b) result = 16'd1;
+ 4'b0000: result = a + b; // add
+ 4'b0001: result = a - b; // sub
+ 4'b0010: result = ~a;
+ 4'b0011: result = a<<b;
+ 4'b0100: result = a>>b;
+ 4'b0101: result = a & b; // and
+ 4'b0110: result = a | b; // or
+ 4'b0111: begin if (a<b) result = 16'd1;
     else result = 16'd0;
     end
  default:result = a + b; // add
@@ -30,4 +30,3 @@ end
 assign zero = (result==16'd0) ? 1'b1: 1'b0;
  
 endmodule
-
--- 16_Bit_RISC_Processor_Icarus-Verilog_Code/RegisterFile.v
+++ 16_Bit_RISC_Processor_Icarus-Verilog_Code/RegisterFile.v
@@ -4,52 +4,50 @@
 // Verilog code for RISC Processor 
 // Verilog code for register file
 module GPRs(
- input    clk,
- // write port
- input    reg_write_en,
- input  [2:0] reg_write_dest,
- input  [15:0] reg_write_data,
- //read port 1
- input  [2:0] reg_read_addr_1,
- output  [15:0] reg_read_data_1,
- //read port 2
- input  [2:0] reg_read_addr_2,
- output  [15:0] reg_read_data_2
+    input    clk,
+    input    reset,
+    // write port
+    input    reg_write_en,
+    input  [2:0] reg_write_dest,
+    input  [15:0] reg_write_data,
+    //read port 1
+    input  [2:0] reg_read_addr_1,
+    output  [15:0] reg_read_data_1,
+    //read port 2
+    input  [2:0] reg_read_addr_2,
+    output  [15:0] reg_read_data_2
 );
- reg [15:0] reg_array [0:7];
- integer i;
+    reg [15:0] reg_array [0:7];
+    integer i;
 
- wire[15:0] reg0;
- wire[15:0] reg1;
- wire[15:0] reg2;
- wire[15:0] reg3;
- wire[15:0] reg4;
- wire[15:0] reg5;
- wire[15:0] reg6;
- wire[15:0] reg7;
- assign reg0 = reg_array[0];
- assign reg1 = reg_array[1];
- assign reg2 = reg_array[2];
- assign reg3 = reg_array[3];
- assign reg4 = reg_array[4];
- assign reg5 = reg_array[5];
- assign reg6 = reg_array[6];
- assign reg7 = reg_array[7];
- // write port
- //reg [2:0] i;
- initial begin
-  for(i=0;i<8;i=i+1)
-   reg_array[i] <= 16'd0;
- end
- always @ (posedge clk ) begin
-   if(reg_write_en) begin
-    reg_array[reg_write_dest] <= reg_write_data;
-   end
- end
- 
+    wire[15:0] reg0;
+    wire[15:0] reg1;
+    wire[15:0] reg2;
+    wire[15:0] reg3;
+    wire[15:0] reg4;
+    wire[15:0] reg5;
+    wire[15:0] reg6;
+    wire[15:0] reg7;
+    assign reg0 = reg_array[0];
+    assign reg1 = reg_array[1];
+    assign reg2 = reg_array[2];
+    assign reg3 = reg_array[3];
+    assign reg4 = reg_array[4];
+    assign reg5 = reg_array[5];
+    assign reg6 = reg_array[6];
+    assign reg7 = reg_array[7];
 
- assign reg_read_data_1 = reg_array[reg_read_addr_1];
- assign reg_read_data_2 = reg_array[reg_read_addr_2];
+    // write port
+    always @ (posedge clk ) begin
+        if(reset) begin
+            for(i=0;i<8;i=i+1) begin
+                reg_array[i] <= 16'd0;
+            end
+        end else if(reg_write_en) begin
+            reg_array[reg_write_dest] <= reg_write_data;
+        end
+    end
 
-
+    assign reg_read_data_1 = reg_array[reg_read_addr_1];
+    assign reg_read_data_2 = reg_array[reg_read_addr_2];
 endmodule
